f980db629b4ba278ff8d86c82f6205fe606d36a0
mkbhru <mkbhru@gmail.com>
2025-03-19 10:53:21 +0530

    final1

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,4 @@
 // const String API_BASE_URL = "http://10.0.2.2:5114/api";
-const String API_BASE_URL = "http://10.0.10.43:5114/api";
+// const String API_BASE_URL = "http://10.0.10.43:5114/api";
+const String API_BASE_URL = "http://10.0.5.38:3000/api";
 // const String API_BASE_URL = "http://10.0.10.24:5114/api";
