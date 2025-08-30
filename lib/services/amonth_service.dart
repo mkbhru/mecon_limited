7e8fe10d897a55351ab5a265be2345c481c02462
mkbhru <mkbhru@gmail.com>
2025-08-30 19:35:10 +0530

    monthly attendance fix

--- lib/services/amonth_service.dart
+++ lib/services/amonth_service.dart
@@ -5,13 +5,11 @@ import '../utils/constants.dart';
 class ApiService {
   Future<List<dynamic>> fetchAttendance(
       String persNo, int year, int month) async {
-    // Print API base URL for debugging
-    print("API_BASE_URL = $API_BASE_URL");
 
+    // print("API_BASE_URL = $API_BASE_URL");
     final url =
         Uri.parse("$API_BASE_URL/attendance/AMonth/attendance-month/$persNo/$year/$month");
 
-    print("Full URL = $url"); // optional, prints full request URL
 
     final response = await http.get(url);
 
