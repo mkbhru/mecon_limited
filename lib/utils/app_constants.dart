cb83d3b38676925c4974bd7ac80408336c69cfef
mkbhru <mkbhru@gmail.com>
2025-10-16 18:04:30 +0530

    time for 10s

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,6 +1,6 @@
 // App-wide constants
 // const Duration updateCheckDelay = Duration(minutes: 1);
-const Duration updateCheckDelay = Duration(seconds: 30);
+const Duration updateCheckDelay = Duration(seconds: 10);
 
 // API endpoints
 const String updateCheckEndpoint = '/version/info';
